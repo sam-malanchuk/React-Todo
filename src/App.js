574a3854e37a350877ba57d67775048558a63d73
sam-malanchuk <malanchuksam@gmail.com>
2019-06-04 20:14:05 -0400

    created Class components and methods so that a new task is created in the object in state when submit/enter is pressed

--- src/App.js
+++ src/App.js
@@ -2,22 +2,37 @@ import React from 'react';
 import TodoForm from './components/TodoComponents/TodoForm';
 import TodoList from './components/TodoComponents/TodoList';
 
+const toDoList= [];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   constructor(props) {
     super();
     this.state = {
-      toDoData: []
+      toDoList: toDoList,
+      toDoItem: ""
     }
   }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  submitHandler = event => {
+    event.preventDefault();
+    const newItem = {
+      task: this.state.toDoItem,
+      id: + new Date(),
+      completed: false
+    }
+    this.setState({toDoList: [...this.state.toDoList, newItem]});
+  }
+  changeHandler = event => {
+    this.setState({toDoItem: event.target.value});
+  }
   render() {
     return (
       <div>
           <h2>Welcome to your Todo App!</h2>
         <TodoList /> 
-        <TodoForm />
+        <TodoForm passChangeHandler={this.changeHandler} passSubmitHandler={this.submitHandler}/>
       </div>
     );
   }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
-class TodoForm extends React.Component {
+function TodoForm(props) {
     // you will need a place to store your state in this component.
     // design `App` to be the parent component of your application.
     // this component is going to take care of state, and any change handlers you need to work with your state
-    render() {
-        return (
-        <form>
-            <input type="text" name="task" placeholder="Enter Task.." />
-            <button>Add To-do</button>
-            <button>Clear Completed</button>
-        </form>
-        );
-    }
+    return (
+    <form onSubmit={props.passSubmitHandler}>
+        <input type="text" name="task" onChange={props.passChangeHandler} placeholder="Enter Task.." />
+        <button>Add To-do</button>
+        <button>Clear Completed</button>
+    </form>
+    );
 }
 
 export default TodoForm; 
